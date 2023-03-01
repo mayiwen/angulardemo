d8678e2ae92f0300bb56619fd29247e19bcccb62
mayiwen <i@mayiwen.com>
2023-03-01 22:15:40 +0800

    formarray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
-
+import { FormArray, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FormBuilder]
 })
 export class AppComponent {
   title = 'angulardemo';
+  addForm =  this.fb.group({
+    array: this.fb.array([
+      this.fb.group({
+        name: 'zs',
+        age: '15'
+      }),
+      this.fb.group({
+        name: 'ws',
+        age: '18'
+      }),
+    ]),
+  })
+  arrayForm = this.fb.array([
+    this.fb.group({
+      name: 'zs',
+      age: '15'
+    }),
+    this.fb.group({
+      name: 'ws',
+      age: '18'
+    }),
+  ])
+  get array() {
+    return this.addForm.get('array') as FormArray;
+  }
+  constructor(private fb: FormBuilder) {
+
+  }
+  add() {
+    this.array.push(
+      this.fb.group({
+        name: '15',
+        age: '16'
+      })
+    )
+  }
 }
