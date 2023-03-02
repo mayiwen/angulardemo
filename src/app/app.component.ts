94b4ddb32b08327206e9026058eb33cb7c575b67
mayiwen <i@mayiwen.com>
2023-03-02 22:42:14 +0800

    333

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,76 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [FormBuilder]
+  providers: [FormBuilder],
 })
 export class AppComponent {
   title = 'angulardemo';
-  addForm =  this.fb.group({
+  addForm = this.fb.group({
     array: this.fb.array([
       this.fb.group({
         name: 'zs',
-        age: '15'
+        age: '15',
+        array: this.fb.array([
+          this.fb.group({
+            hello: 1,
+            test: 2,
+          }),
+        ]),
       }),
       this.fb.group({
         name: 'ws',
-        age: '18'
+        age: '18',
+        array: this.fb.array([]),
       }),
     ]),
-  })
-  arrayForm = this.fb.array([
-    this.fb.group({
-      name: 'zs',
-      age: '15'
-    }),
-    this.fb.group({
-      name: 'ws',
-      age: '18'
-    }),
-  ])
+  });
   get array() {
     return this.addForm.get('array') as FormArray;
   }
-  constructor(private fb: FormBuilder) {
-
-  }
+  constructor(private fb: FormBuilder) {}
   add() {
     this.array.push(
       this.fb.group({
         name: '15',
-        age: '16'
+        age: '16',
+        array: this.fb.array([
+          this.fb.group({
+            hello: 1,
+            test: 2,
+          }),
+        ]),
       })
-    )
+    );
+  }
+  itemtt(item: any) {
+    console.log(item);
+    let array = item.get('array') as FormArray;
+   
+    console.log(array)
+    for (let index = array.value.length - 1; index >= 0; index--) {
+      array.removeAt(index)
+    }
+    setTimeout(() => {
+      let arr = [
+        {
+          hello: 3,
+          test: 4,
+        },
+        {
+          hello: 5,
+          test: 6,
+        },
+      ];
+
+      array.push(this.fb.group({
+        hello: 5,
+        test: 6,
+      }));
+      arr.forEach(item => array.push(this.fb.group(item)))
+      // array.push(this.fb.group(arr.map(item => this.fb.group(item))));
+    }, 200);
   }
 }
